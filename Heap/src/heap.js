dba22a05294bf8ebdeae4e82772ef6764ac8a989
Drew Cuthbertson <syeoryn@gmail.com>
2014-06-16 21:12:47 -0700

    Implements heap extract

--- Heap/src/heap.js
+++ Heap/src/heap.js
@@ -33,8 +33,32 @@ Heap.prototype.peek = function() {
 };
 
 Heap.prototype.insert = function(element) {
+  var i = this._data.length;
+  this._data.push(element);
+  var parentIndex = this._parentOf(i)
+  console.log(element)
+  while( element < this._data[parentIndex] ){
+    this._swap(i, parentIndex);
+    i = parentIndex;
+    parentIndex = this._parentOf(i);
+  }
 };
 
 Heap.prototype.extract = function() {
+  var last = this._data[0];
+  this._data[0] = this._data.pop();
+  var left = this._leftChildOf(0);
+  var right = this._rightChildOf(0);
+  var low = this._data[right] < this._data[left] ? right : left;
+  var index = 0;
+  while( this._data[index] > this._data[low] ){
+    this._swap(index,low);
+    index = low;
+    left = this._leftChildOf(index);
+    right = this._rightChildOf(index);
+    low = this._data[left] < this._data[right] ? left : right;
+  }
+
+  return last;
 };
 
